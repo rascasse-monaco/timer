cabb747f87f156ec6b3f236f7088485155d38148
rascasse-monaco <mktwkhr@gmail.com>
2020-12-31 02:29:21 +0900

    モジュール化の練習

--- timer.js
+++ timer.js
@@ -1,36 +1,16 @@
 'use strict'
+import { makeDoubleDigits, countToTime } from './time-handling-util.js';
+
 let count = 0;
 let setIntervalID = null;
-/**
- * conuntを時間に変換
- * @param {Number} 1/10秒の数を入れる。
- * @return {Number}
- */
-function countToTime(count) {
-  return {
-    sec: Math.floor(count / 10 % 60),//分を返す60秒になったら0秒にする
-    min: Math.floor(count / (60*10) % 60),//60秒毎に分を返す60分になったら0分にする
-    hour: Math.floor(count / (60*60*10) % 24)//60分に時間を返す24時間になったら00時間にする
-  }
-}
-/**
- * 10の位に0を挿入して数字の桁数を合わせる
- * @return {String} 0を足した文字列
- * @param {Number} num 秒、分など
- */
-function makeDoubleDigits(num){
-  num += '';
-  if (num.length === 1) {
-    return num = `0${num}`;
-  } else {
-    return num;
-  }
-}
+
 //スタートボタンの動作。
 function startBtn() {
-    replaceButton('button', 'start','button', 'startDeactivate', '','Start');
-    replaceButton('button', 'stopDeactivate','button', 'stop', 'stop(setIntervalID)','Stop');
+    replaceButton('button', 'start','button', 'startDeactivate', 'Start');
+    replaceButton('button', 'stopDeactivate','button', 'stop', 'Stop');
     start();
+    const stopButton = document.getElementById('stop');
+    stopButton.addEventListener( 'click', stop, false );
 }
 
 function start() {
@@ -44,25 +24,32 @@ function start() {
     `${makeDoubleDigits(countToTime(count).hour)}`
   }, 100);
 }
-function stop(intervalID) {
-  replaceButton('button', 'startDeactivate','button', 'start', 'startBtn()','Start');
-  replaceButton('button', 'stop','button', 'stopDeactivate', '','Stop');
-  clearInterval(intervalID);
+function stop() {
+  replaceButton('button', 'startDeactivate','button', 'start', 'Start');
+  replaceButton('button', 'stop','button', 'stopDeactivate','Stop');
+  clearInterval(setIntervalID);
+  const startButton = document.getElementById('start');
+  startButton.addEventListener( 'click', startBtn, false );
 }
 
 function reload() {
   location.reload();
 }
 
-function replaceButton(parentID, oldChildID, createElement, newChildID, onclick, innerHTMLtext) {
+function replaceButton(parentID, oldChildID, createElement, newChildID, innerHTMLtext) {
   const parentElement = document.getElementById(`${parentID}`);
   const oldChild = document.getElementById(`${oldChildID}`)
   const newChild = document .createElement(`${createElement}`);
   newChild.setAttribute('type', 'button');
   newChild.setAttribute('class', 'button');
   newChild.setAttribute('id', `${newChildID}`);
-  newChild.setAttribute('onclick', `${onclick}`);
   newChild.innerHTML = innerHTMLtext;
        
   parentElement.replaceChild(newChild, oldChild);
 }
+
+const startButton = document.getElementById('start');
+startButton.addEventListener( 'click', startBtn, false );
+
+const reloadButton = document.getElementById('reload');
+reloadButton.addEventListener( 'click', reload, false );
